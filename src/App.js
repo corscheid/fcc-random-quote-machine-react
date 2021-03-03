5ebe5c9362781fbe577af82f52ec3a88ba4fc4bb
Corey Scheideman <corscheid@gmail.com>
2021-03-03 03:58:20 -0600

    Move fetchData declaration out of useEffect hook

--- src/App.js
+++ src/App.js
@@ -13,18 +13,19 @@ const App = () => {
     setQuote(newQuote);
   };
 
+  const fetchData = async () => {
+    const quotesURL =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+    const response = await fetch(quotesURL);
+    const quotes = await response.json();
+    const idx = Math.floor(Math.random() * quotes.quotes.length);
+    const newQuote = quotes.quotes[idx];
+    setQuoteList(quotes.quotes);
+    setQuote(newQuote);
+    setLoading(false);
+  };
+
   React.useEffect(() => {
-    const fetchData = async () => {
-      const quotesURL =
-        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-      const response = await fetch(quotesURL);
-      const quotes = await response.json();
-      const idx = Math.floor(Math.random() * quotes.quotes.length);
-      const newQuote = quotes.quotes[idx];
-      setQuoteList(quotes.quotes);
-      setQuote(newQuote);
-      setLoading(false);
-    };
     fetchData();
   }, []);
 
